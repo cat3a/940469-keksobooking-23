2838c5fc223924541d6936eef45d0d2826249490
cat3a <cat3a-88@mail.ru>
2021-05-29 22:04:55 +0300

    Заканчивет черновой вариант функций случайного числа и случайной координаты

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-const getRandomInteger = function (min, max) {
-  if(min <= max || min < 0 || max < 0) {
-    throw new Error('Оба числа должны быть неотрицательными. Числа не могут быть равны друг другу.');
+const getRandomInteger = function (minNumber, maxNumber) {
+  if(minNumber >= maxNumber || minNumber < 0 || maxNumber < 0) {
+    throw new Error('Оба числа должны быть неотрицательными. Первое число должно быть меньше второго.');
   }
-  const number = min - 0.5 + Math.random() * (max - min + 1);
+  const minNumberRounding = Math.ceil(minNumber);
+  const maxNumberRounding = Math.floor(maxNumber);
+  const number = minNumberRounding - 0.5 + Math.random() * (maxNumberRounding - minNumberRounding + 1);
   return Math.round(number);
 };
 
 getRandomInteger(6, 9);
-getRandomInteger(6, 90);
-getRandomInteger(90, 90);
+
+const getRandomCoordinate =  function (minNumber, maxNumber, rounding) {
+  if(minNumber < 0 || maxNumber < 0 || minNumber >= maxNumber || rounding < 0 || rounding > 100) {
+    throw new Error('Все числа должны быть неотрицательными. Первое число должно быть меньше второго. Третье число должно находиться в диапазоне [0, 100].');
+  }
+  const number = (Math.random() * (maxNumber - minNumber) + minNumber);
+  return Number(number.toFixed(rounding));
+};
+
+getRandomCoordinate(6.77, 9.5, 9);
