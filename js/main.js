16c8ebcbe6c8997194ed555083933ce5b64ed74f
cat3a <cat3a-88@mail.ru>
2021-06-07 00:16:45 +0300

    Добавляет генерацию первого объекта

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const getRandomInteger = (minNumber, maxNumber) => {
-  if(minNumber >= maxNumber || minNumber < 0) {
+  if (minNumber >= maxNumber || minNumber < 0) {
     throw new Error('Первое число должно быть меньше второго. Оба числа должны быть неотрицательными.');
   }
   minNumber = Math.ceil(minNumber);
@@ -11,7 +11,7 @@ const getRandomInteger = (minNumber, maxNumber) => {
 getRandomInteger(6, 9);
 
 const getRandomCoordinate = (minNumber, maxNumber, rounding) => {
-  if(minNumber < 0 || minNumber >= maxNumber) {
+  if (minNumber < 0 || minNumber >= maxNumber) {
     throw new Error('Все числа должны быть неотрицательными. Первое число должно быть меньше второго.');
   }
   const number = (Math.random() * (maxNumber - minNumber) + minNumber);
@@ -19,3 +19,38 @@ const getRandomCoordinate = (minNumber, maxNumber, rounding) => {
 };
 
 getRandomCoordinate(6.77, 9.5, 9);
+
+
+const SIMILAR_OBJECT_COUNT = 10;
+const AUTHOR_START_COUNT = 1;
+const AUTHOR_END_COUNT = 8;
+const NO_AVATAR = 'no-avatar';
+
+const getAvatar = () => `img/avatars/user0${getRandomInteger(AUTHOR_START_COUNT, AUTHOR_END_COUNT)}.png`;
+
+const similarAvatarObjects = [];
+for (let i = AUTHOR_START_COUNT; i <= AUTHOR_END_COUNT; i++) {
+  let avatar = getAvatar();
+  while (similarAvatarObjects.find((elem) => elem === avatar)) {
+    avatar = getAvatar();
+  }
+  similarAvatarObjects.push(avatar);
+}
+while (similarAvatarObjects.length < SIMILAR_OBJECT_COUNT) {
+  similarAvatarObjects.push(NO_AVATAR);
+}
+
+const getAuthor = () => {
+  const author = similarAvatarObjects[0];
+  similarAvatarObjects.splice(0,1);
+  return author;
+};
+
+const getObject = () =>
+  ({
+    author: {avatar: getAuthor()},
+  });
+
+const similarObjects = new Array(SIMILAR_OBJECT_COUNT).fill(null).map(() => getObject());
+
+console.log(similarObjects);
