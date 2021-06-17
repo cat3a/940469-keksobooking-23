a9e965a04170046c577d12f1f2546ab84c6e45f2
cat3a <cat3a-88@mail.ru>
2021-06-18 00:47:01 +0300

    Заменяет однотипные условия для проверки на функцию

--- js/generation-data.js
+++ js/generation-data.js
@@ -1,6 +1,7 @@
 //TODO: Это модуль генерации разметки похожих элементов.
 
 import {similarObjects} from './map-data.js';
+import {getVerification} from './utils.js';
 
 const HOME_TYPES = {
   'flat': 'Квартира',
@@ -14,20 +15,14 @@ const template = document.querySelector('#card').content;
 const templateDraw = document.querySelector('#map-canvas');
 const similarFragment = document.createDocumentFragment();
 
-for (let i = 0; i <= similarObjects.length-1; i++) {
+for (let i = 0; i <= similarObjects.length - 1; i++) {
   const ticket = template.cloneNode(true);
   ticket.querySelector('.popup__title').textContent = similarObjects[i].offer.title;
-  if (similarObjects[i].offer.title.length < 2) {
-    ticket.querySelector('.popup__title').classList.add('hidden');
-  }
+  getVerification('.popup__title', ticket, similarObjects[i].offer.title.length < 2);
   ticket.querySelector('.popup__text--address').textContent = similarObjects[i].offer.address;
-  if (similarObjects[i].offer.address.length < 10)  {
-    ticket.querySelector('.popup__text--address').classList.add('hidden');
-  }
+  getVerification('.popup__text--address', ticket, similarObjects[i].offer.address.length < 10);
   ticket.querySelector('.popup__text--price').textContent = `${similarObjects[i].offer.price} ₽/ночь`;
-  if (similarObjects[i].offer.price < 1) {
-    ticket.querySelector('.popup__text--price').classList.add('hidden');
-  }
+  getVerification('.popup__text--price', ticket, similarObjects[i].offer.price < 1);
   ticket.querySelector('.popup__type').textContent = HOME_TYPES[similarObjects[i].offer.type];
   ticket.querySelector('.popup__text--capacity').textContent = `${similarObjects[i].offer.rooms} комнаты для ${similarObjects[i].offer.guests} гостей`;
   ticket.querySelector('.popup__text--time').textContent = `Заезд после ${similarObjects[i].offer.checkin}, выезд до ${similarObjects[i].offer.checkout}`;
@@ -41,14 +36,10 @@ for (let i = 0; i <= similarObjects.length-1; i++) {
     ticketFeatureElement.classList.add(`popup__feature--${ticketFeature}`);
     ticketFeatureSelector.appendChild(ticketFeatureElement);
   }
-  if (similarObjects[i].offer.features.length === 0) {
-    ticket.querySelector('.popup__features').classList.add('hidden');
-  }
+  getVerification('.popup__features', ticket, similarObjects[i].offer.features.length === 0);
 
   ticket.querySelector('.popup__description').textContent = similarObjects[i].offer.description;
-  if (similarObjects[i].offer.description.length < 1) {
-    ticket.querySelector('.popup__description').classList.add('hidden');
-  }
+  getVerification('.popup__description', ticket, similarObjects[i].offer.description.length < 1);
 
   const ticketPhotosSelector = ticket.querySelector('.popup__photos');
   ticketPhotosSelector.querySelector('img').remove();
@@ -61,14 +52,10 @@ for (let i = 0; i <= similarObjects.length-1; i++) {
     ticketPhotoElement.alt = 'Фотография жилья';
     ticketPhotosSelector.appendChild(ticketPhotoElement);
   }
-  if (similarObjects[i].offer.photos.length === 0) {
-    ticket.querySelector('.popup__photos').classList.add('hidden');
-  }
+  getVerification('.popup__photos', ticket, similarObjects[i].offer.photos.length === 0);
 
   ticket.querySelector('.popup__avatar').src = similarObjects[i].author.avatar;
-  if (similarObjects[i].author.avatar.length === 0) {
-    ticket.querySelector('.popup__avatar').classList.add('hidden');
-  }
+  getVerification('.popup__avatar', ticket, similarObjects[i].author.avatar.length === 0);
 
   similarFragment.appendChild(ticket);
   if (i === 0) {
