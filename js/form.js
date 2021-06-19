10cc6a29106d0f8b10adad2d817cb8e7cc849f88
cat3a <cat3a-88@mail.ru>
2021-06-19 18:32:58 +0300

    Преобразует 2 функции по инициализации формы в одну. Превращает обработчик события click в одноразовый.

---
+++ js/form.js
@@ -0,0 +1,17 @@
+const ticketForm = document.querySelector('.ad-form');
+const filterForm = document.querySelector('.map__filters');
+const ticketFormChildren = ticketForm.querySelectorAll('fieldset');
+const filterFormChildren = filterForm.children;
+
+//TODO: Я пока не знаю когда и как должна сработать инициализация карты, поэтому пусть будет пока так:
+
+const  disableForm = (selector='ad-form--disabled', token='disabled', boolean= false) => {
+  ticketForm.classList.toggle(selector);
+  ticketFormChildren.forEach((fieldset) => fieldset.toggleAttribute(token, boolean));
+  filterForm.classList.toggle(selector);
+  Array.from(filterFormChildren).forEach((fieldset) => fieldset.toggleAttribute(token, boolean));
+};
+
+disableForm('ad-form--disabled', 'disabled', true);
+
+document.addEventListener('click', ()=> {disableForm();}, { once: true });
